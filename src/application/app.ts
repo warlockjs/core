da4977434ad2009868f0b8c7b5f061a89b07d684
Hasan Zohdy <hassanzohdy@gmail.com>
2026-04-18 14:55:24 +0200

    "Core: Introduced app instance"

--- src/application/app.ts
+++ src/application/app.ts
@@ -1,16 +1,35 @@
+import { type DataSource } from "@warlock.js/cascade";
+import { type FastifyInstance } from "fastify";
+import { type Server } from "socket.io";
+import { type Router } from "../router";
 import { container } from "./../container";
 
 export const app = {
-  get socket() {
+  /**
+   * Socket Io Instance
+   * Available only if socket.io config file exists
+   */
+  get socket(): Server {
     return container.get("socket");
   },
-  get http() {
+  /**
+   * HTTP Server Instance
+   * Available only if http config file exists
+   */
+  get http(): FastifyInstance {
     return container.get("http.server");
   },
-  get router() {
+  /**
+   * Router Instance
+   */
+  get router(): Router {
     return container.get("router");
   },
-  get database() {
+  /**
+   * Database Instance
+   * Available only if database config file exists
+   */
+  get database(): DataSource {
     return container.get("database.source");
   },
 };
